f16dfa9f396fe8b645902285fe68a60d84e1442a
Vamsi.ch <vamsich.ext@deloitte.com>
2024-06-29 12:25:42 +0530

    package_name_changed

--- android/app/src/main/java/com/nivaas/MainActivity.kt
+++ android/app/src/main/java/com/nivaas/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nivas
+package com.nivaas
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "nivas"
+  override fun getMainComponentName(): String = "nivaas"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/nivasTests/nivasTests.m
+++ ios/nivasTests/nivasTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nivasTests : XCTestCase
+@interface nivaasTests : XCTestCase
 
 @end
 
-@implementation nivasTests
+@implementation nivaasTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
